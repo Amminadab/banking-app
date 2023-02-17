d0759e77ac3a3ba1d3ba6ea8f1c0f84a520219da
Amminadab <amminadabeliasfant@gmail.com>
2023-02-17 15:25:55 +0300

    created multiple accounts testing data

---
+++ src/accounts-data.js
@@ -0,0 +1,40 @@
+const ACCOUNT_DATA = [
+  {
+    accountNumber: "1",
+    name: "Brown",
+    FatherName: "Eyobed",
+    age: "24",
+    transaction: [300, 9000, -600, -5000, 700, 800, 2800, -4200],
+    accountType: "saving",
+    pin: "1234",
+  },
+  {
+    accountNumber: "2",
+    name: "Heryakos",
+    FatherName: "Beanie",
+    age: "18",
+    transaction: [30000, 900, -6000, -5000, 7000, -8000, 5000, -8200],
+    accountType: "salary",
+    pin: "1234",
+  },
+  {
+    accountNumber: "3",
+    name: "Amminadab",
+    FatherName: "Cowboy",
+    age: "26",
+    transaction: [6000, 900, -400, -900, 12000, -800, 2800, -4200],
+    accountType: "fixed",
+    pin: "1234",
+  },
+  {
+    accountNumber: "4",
+    name: "Bineyam",
+    FatherName: "Brim",
+    age: "60",
+    transaction: [6300, 9000, -6000, -5000, 900, 2800, 9800, -7200],
+    accountType: "recurring",
+    pin: "1234",
+  },
+];
+
+export default ACCOUNT_DATA;

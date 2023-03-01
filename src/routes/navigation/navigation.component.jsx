dd82223790bc430bebf5bdaf8244277501648663
Amminadab <amminadabeliasfant@gmail.com>
2023-03-02 01:28:42 +0300

    final touches 3

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { AccountContext } from "../../context/account/account.context";
 import { UserContext } from "../../context/user/user.context";
@@ -21,6 +21,14 @@ const Navigation = () => {
   const buttonManagement = () => {
     setClicked(false);
   };
+
+  useEffect(() => {
+    if (clicked) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "";
+    }
+  }, [clicked]);
 
   return (
     <>
